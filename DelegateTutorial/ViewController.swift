a618dd48894a0630a57c9664dc3fb514edec6a93
Nancy Jain <nancyjain0631@gmail.com>
2022-09-27 10:20:03 +0530

    Initial Commit

---
+++ DelegateTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateTutorial
+//
+//  Created by Nancy Jain on 27/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
